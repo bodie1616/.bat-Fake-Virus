57545d7ff9daea7a0b98331bcc13170b148b2e69
bodie1616 <120243516+bodie1616@users.noreply.github.com>
2022-12-09 23:56:19 -0800

    Fake Virus Finished

---
+++ Regret everything.bat
@@ -0,0 +1,197 @@
+@echo off
+title  Do You Regret Everthing?
+color C
+echo This malware is no joke do you want to run it still? Type Yes or No
+set input=
+set /p input=
+if %input%== Yes goto 1
+if %input%== No goto 2
+:1
+title Malware Activated!
+echo Deleting Files...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Now i will inject my other malware and virus called YOU ARE STUPID HAHAHA
+ping n- 2 127.0.0.1>nul
+echo This May Take A Minute...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Done!
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Do You Want Further Destruction To Be Activated?
+echo Type 1 for yes or 2 for no
+set input=
+set /p input=
+if %input%== 1 goto 3
+if %input%== 2 goto 4
+:4
+exit
+
+
+
+
+
+
+
+:2
+title Good
+color A
+echo Ok..This may take a sec to cancel all of this dont worry it will be less than a minute or 30 seconds
+ping n- 2 127.0.0.1>nul
+exit
+
+
+
+
+
+
+
+
+
+
+:3
+echo Why Tho?
+ping n- 2 127.0.0.1>nul
+START C:\Users\bodie\Videos\Happy.jaw.zip\happy.jaw.exe
+START C:\Users\bodie\Music\kill\ok.vbs
+START C:\Users\bodie\Music\kill\DIE.txt
+START C:\Users\bodie\Music\kill\DIE.txt
+START C:\Users\bodie\Music\kill\DIE.txt
+START C:\Users\bodie\Music\kill\DIE.txt
+START C:\Users\bodie\Music\kill\DIE.txt
+START C:\Users\bodie\Music\kill\DIE.txt
+echo How Do You Like My Malware?
+echo Do You Want More? Yes Or No
+set input=
+set /p input=
+if %input%== Yes goto 8
+if %input%== No goto 4
+:8
+echo Are You Sure
+set input=
+set /p input=
+if %input%== Yes goto 10
+if %input%== No goto 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:10
+Color D
+echo Let me do my thing dont close the program!
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+START C:\Users\bodie\Videos\CIH\CIH\CIH\bin\Debug\CIH.exe
+
+echo Do You Want  even More
+set input=
+set /p input=
+if %input%== Yes goto 30
+if %input%== No goto 4
+
+
+
+
+
+
+
+
+
+:30
+echo Ok!
+ping n- 2 127.0.0.1>nul
+color A
+title 101010010100101010100101010010100101010100101001010101010100101
+start "You Are An Idiot" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+https://www.youtube.com/watch?v=48rz8udZBmQ
+start "You Are An Idiot" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+https://www.youtube.com/watch?v=48rz8udZBmQ
+start "You Are An Idiot" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+https://www.youtube.com/watch?v=48rz8udZBmQ
+start "You Are An Idiot" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+https://www.youtube.com/watch?v=48rz8udZBmQ
+start "You Are An Idiot" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+https://www.youtube.com/watch?v=48rz8udZBmQ
+echo Do You Want To Continue?
+set input=
+set /p input=
+if %input%== Yes goto 70
+if %input%== No goto 4
+
+
+
+
+
+
+
+:70
+echo If You Have Made It This Far Enter Yes If You Have Not Enter No
+set input=
+set /p input=
+if %input%== Yes goto 4
+if %input%== No goto 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
